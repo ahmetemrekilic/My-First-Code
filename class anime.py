b4dcd9381646e0eaa9a9e88e9ec648709a6b701a
Ahmet Emre KILIC <64087450+ahmetemrekilic@users.noreply.github.com>
2021-03-11 00:17:28 +0300

    Add files via upload

---
+++ class anime.py
@@ -0,0 +1,51 @@
+import time
+
+class Anime():
+
+    def __init__(self,name,episode,starting_date):
+        self.name=name
+        self.episode=episode
+        self.starting_date=starting_date
+
+
+class Anime1(Anime):
+    def __init__(self,name,episode,starting_date,type):
+        super().__init__(name,episode,starting_date)
+        self.type=type
+
+    def show_anime_information(self,):
+        print(""""
+        Anime name:             {}
+        Anime episode:          {}
+        Anime starting_date:    {}
+        Anime type:             {}
+        """.format(self.name,self.episode,self.starting_date,self.type))
+
+    def __str__(self):
+        return ("Anime name:{}\nAnime episode:{}\nAnime starting_date:{}\nAnime type:{}\n".format(self.name,self.episode,self.starting_date,self.type))
+
+    def add_episode(self,ep):
+        print("Episode uploading..")
+        time.sleep(2)
+        self.episode+=ep
+
+    def add_type(self,ty):
+        print("Type uploading..")
+        time.sleep(2)
+        self.type.append(ty)
+
+anime1=Anime1("Black Clover",170,2017,"Supernatural")
+anime2=Anime1("One Piece",900,1998,["Advanture"])
+anime1.show_anime_information()
+
+anime2.add_episode(65)
+anime2.add_type("Supernatural")
+anime2.show_anime_information()
+
+#YOU CAN ADD NEW OBJECT
+#YOU CAN ADD İNFORMATİON WITH FUNCTIONS
+
+
+
+
+
